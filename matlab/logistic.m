3a4eb34290b1a46daee677be5ee328d3f127c144
Edoardo Vacchini <edoardo.vacchini01@universitadipavia.it>
2021-09-11 13:51:29 +0200

    Add knn and bins method

--- matlab/logistic.m
+++ matlab/logistic.m
@@ -1,5 +1,9 @@
 function power = logistic(windspeed, theta)
     out = theta(1)./(1 + theta(2)*exp(- theta(3)*windspeed));
+    
+    temp = out(out > 3690);
+    out(out > 3690) = 3690 * ones(length(temp), 1);
+    
 	power = out;
 end
 
